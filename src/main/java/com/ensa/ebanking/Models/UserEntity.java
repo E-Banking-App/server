e2a7e2a4537eff481085d95d3ba4def277ed0bf1
nizarkrimis <nizar_id@live.fr>
2023-06-09 01:11:00 +0100

    fix erorr

--- src/main/java/com/ensa/ebanking/Models/UserEntity.java
+++ src/main/java/com/ensa/ebanking/Models/UserEntity.java
@@ -19,7 +19,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
+//@Builder
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class UserEntity implements UserDetails {
     @Id
